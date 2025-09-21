e793e8cde60d1f3a9cecb8d1ec6005e8a3b4d838
Sergey Palatov <94283888+supernagibator2007@users.noreply.github.com>
2025-09-21 17:42:30 +0300

    correct main.py
    Deleted space

--- 1lab/main.py
+++ 1lab/main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     string = str(input()).lower()
     words = string.split(" ")
     stripped_words = [str(word.strip(',').strip('.').strip('!').strip('?')) for word in words if word != '-']
-    letters =  list(''.join(stripped_words))
+    letters = list(''.join(stripped_words))
     print(max(stripped_words, key = stripped_words.count))
     print(max(letters, key = letters.count))
