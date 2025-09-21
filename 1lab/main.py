d04082cb49d7e56d0d706d4af5a620e680456c63
supernagibator2007 <serg25102007@gmail.com>
2025-09-21 17:30:05 +0300

    max count -> 5 max count

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,9 @@
 
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+    string = str(input()).lower()
+    words = string.split(" ")
+    stripped_words = [str(word.strip(',').strip('.').strip('!').strip('?')) for word in words if word != '-']
+    letters =  list(''.join(stripped_words))
+    print(max(stripped_words, key = stripped_words.count))
+    print(max(letters, key = letters.count))
