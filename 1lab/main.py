3dcfea1139fdca263b48795bee6dbe36bd0f8e89
supernagibator2007 <serg25102007@gmail.com>
2025-10-06 22:29:45 +0300

    Не выводит пустую строку и двойные пробелы

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,32 +1,33 @@
 
-
 if __name__ == "__main__":
     string = str(input()).lower()
+    if (len(string) > 0 and string.count(' ') != len(string)):
+        unstripped_words = string.split(" ")
+        
+        word = [str(word.strip(',').strip('.').strip('!').strip('?').strip(':')).strip() \
+                         for word in unstripped_words if word != '-']
+        words = [i for i in word if i]
+        letters =  list(''.join(words))
+        
+        sorted_letters = sorted(letters, key = letters.count, reverse = True)
+        sorted_words = sorted(words, key = words.count, reverse = True)
 
-    unstripped_words = string.split(" ")
-    words = [str(word.strip(',').strip('.').strip('!').strip('?').strip(':')) \
-                       for word in unstripped_words if word != '-']  
-    letters =  list(''.join(words))
-    
-    sorted_letters = sorted(letters, key = letters.count, reverse = True)
-    sorted_words = sorted(words, key = words.count, reverse = True)
+        set_letters = sorted(set(sorted_letters), key = sorted_letters.index)
+        set_words = sorted(set(sorted_words), key = sorted_words.index)
 
-    set_letters = sorted(set(sorted_letters), key = sorted_letters.index)
-    set_words = sorted(set(sorted_words), key = sorted_words.index)
+        i = 1
+        j = 1
 
-    i = 1
-    j = 1
-
-    while i != 6:
-        try:
-            print(i, 'по популярности слово - ', set_words[i - 1])
-            i += 1
-        except:
-            break
-    
-    while j != 6:
-        try:
-            print(j, 'по популярности буква - ', set_letters[j - 1])
-            j += 1
-        except:
-            break
+        while i != 6:
+            try:
+                print(i, 'по популярности слово - ', set_words[i - 1])
+                i += 1
+            except:
+                break
+        
+        while j != 6:
+            try:
+                print(j, 'по популярности буква - ', set_letters[j - 1])
+                j += 1
+            except:
+                break
