d0a002f60d4cdad48f64a6293793c887e2a811b2
Sergey Palatov <serg25102007@gmail.com>
2025-10-12 23:05:34 +0300

    Окончательно довёл до PEP-8

--- 1lab/main.py
+++ 1lab/main.py
@@ -2,17 +2,17 @@
 if __name__ == "__main__":
     string = str(input()).lower()
     unstripped_words = string.split(" ")
-    
-    word = [str(word.strip(',').strip('.').strip('!').strip('?').strip(':')).strip() \
-                     for word in unstripped_words if word != '-']
-    words = [i for i in word if i]
-    letters =  list(''.join(words))
-        
-    sorted_letters = sorted(letters, key = letters.count, reverse = True)
-    sorted_words = sorted(words, key = words.count, reverse = True)
 
-    set_letters = sorted(set(sorted_letters), key = sorted_letters.index)
-    set_words = sorted(set(sorted_words), key = sorted_words.index)
+    word = [str(word.strip(',').strip('.').strip('!').strip('?').strip(':'))
+            .strip() for word in unstripped_words if word != '-']
+    words = [i for i in word if i]
+    letters = list(''.join(words))
+
+    sorted_letters = sorted(letters, key=letters.count, reverse=True)
+    sorted_words = sorted(words, key=words.count, reverse=True)
+
+    set_letters = sorted(set(sorted_letters), key=sorted_letters.index)
+    set_words = sorted(set(sorted_words), key=sorted_words.index)
 
     if (len(string) > 0 and string.count(' ') != len(string)):
         i = 1
@@ -22,12 +22,12 @@ if __name__ == "__main__":
             try:
                 print(i, 'по популярности слово - ', set_words[i - 1])
                 i += 1
-            except:
+            except IndexError:
                 break
-        
+
         while j != 6:
             try:
                 print(j, 'по популярности буква - ', set_letters[j - 1])
                 j += 1
-            except:
+            except IndexError:
                 break
