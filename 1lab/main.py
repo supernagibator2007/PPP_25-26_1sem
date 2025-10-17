279036605a28a4b8b36a749bdafc8049a4365d68
Sergey Palatov <serg25102007@gmail.com>
2025-10-17 18:41:57 +0300

    Выход из неправильных строк без обработки

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,6 +1,11 @@
+import sys
+
 
 if __name__ == "__main__":
     string = str(input()).lower()
+    if (len(string) == 0 or string.count(' ') == len(string)):
+        sys.exit()
+
     unstripped_words = string.split(" ")
 
     word = [str(word.strip(',').strip('.').strip('!').strip('?').strip(':'))
@@ -14,20 +19,19 @@ if __name__ == "__main__":
     set_letters = sorted(set(sorted_letters), key=sorted_letters.index)
     set_words = sorted(set(sorted_words), key=sorted_words.index)
 
-    if (len(string) > 0 and string.count(' ') != len(string)):
-        i = 1
-        j = 1
+    i = 1
+    j = 1
 
-        while i != 6:
-            try:
-                print(i, 'по популярности слово - ', set_words[i - 1])
-                i += 1
-            except IndexError:
-                break
+    while i != 6:
+        try:
+            print(i, 'по популярности слово - ', set_words[i - 1])
+            i += 1
+        except IndexError:
+            break
 
-        while j != 6:
-            try:
-                print(j, 'по популярности буква - ', set_letters[j - 1])
-                j += 1
-            except IndexError:
-                break
+    while j != 6:
+        try:
+            print(j, 'по популярности буква - ', set_letters[j - 1])
+            j += 1
+        except IndexError:
+            break
