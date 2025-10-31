15a909db02e70fa8179443c3b0794b36548271ff
Sergey Palatov <serg25102007@gmail.com>
2025-10-31 16:18:34 +0300

    Исправил

--- 2lab/main.py
+++ 2lab/main.py
@@ -3,10 +3,17 @@ latin_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 if __name__ == "__main__":
     string = input()
+    counter: dict[str, int] = {}
     if all([1 if symbol in latin_letters else 0 for symbol in string]):
         for letter in string:
-            if string.count(letter.lower()) > string.count(letter.upper()):
-                string = string.replace(letter.lower(), '')
+            if letter in counter.keys():
+                counter[letter] += 1
+            else:
+                counter[letter] = 1
+
+        for letter in counter.keys():
+            if counter[letter.upper()] < counter[letter.lower()]:
                 string = string.replace(letter.upper(), '')
+                string = string.replace(letter.lower(), '')
 
         print(string)
